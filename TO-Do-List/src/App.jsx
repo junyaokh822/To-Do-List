1c15f9b51332941ca876ec72cc9d8e213afba77b
junyaokh822 <junyaokh822@gmail.com>
2023-06-23 17:00:09 -0400

    making change of the style

--- TO-Do-List/src/App.jsx
+++ TO-Do-List/src/App.jsx
@@ -1,39 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [list,setList] = useState([]);
-    const [input,setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
 
-    const addTodo =(todo) =>{
-      const newTodo={
-        id:Math.random(),
-        todo:todo,
-    
-      };
-      setList([...list,newTodo]);
+  const addTodo = (todo) => { //adding new tasks to the list
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    setList([...list, newTodo]);
+    setInput('');
+  };
 
-      setInput("");
+  const deleteTodo = (id) => {   //delete the stored task from the list
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') { // pressing enter triggers the addTodo function
+      addTodo(input);
     }
+  };
 
-    const deleteTodo =(id) =>{
-      const newList=list.filter((todo)=>todo.id!==id);
-      setList(newList);
-    }
-    
-    return(
+  return (
     <div>
-      <h1>Task List</h1>
-      <input type="text" 
-      value={input}  
-      onChange={(e)=> setInput(e.target.value)}/>
-      <button onClick={()=> addTodo(input)}>Add</button>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyPress={handleKeyPress} 
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
       <ul>
-        {list.map((todo)=>(
+        {list.map((todo) => (
           <li key={todo.id}>
-            {todo.todo}
-            <button onClick={()=> deleteTodo(todo.id)}>&times;</button>  
-
+            <span className="bullet">&bull;</span>
+            <span>{todo.todo}</span>
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
           </li>
         ))}
       </ul>
@@ -41,4 +48,5 @@ function App() {
   );
 }
 
-export default App
+export default App;
+
