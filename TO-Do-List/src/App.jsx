564eb20cc3273ff762b8ccd61b734d68d90f9566
junyaokh822 <junyaokh822@gmail.com>
2023-06-23 15:46:49 -0400

    added the delete button for crossing out task

--- TO-Do-List/src/App.jsx
+++ TO-Do-List/src/App.jsx
@@ -2,18 +2,41 @@ import React, {useState} from 'react'
 import './App.css';
 
 function App() {
-  
-
     const [list,setList] = useState([]);
     const [input,setInput] = useState("");
-    return(
 
+    const addTodo =(todo) =>{
+      const newTodo={
+        id:Math.random(),
+        todo:todo,
+    
+      };
+      setList([...list,newTodo]);
+
+      setInput("");
+    }
+
+    const deleteTodo =(id) =>{
+      const newList=list.filter((todo)=>todo.id!==id);
+      setList(newList);
+    }
+    
+    return(
     <div>
       <h1>Task List</h1>
       <input type="text" 
       value={input}  
       onChange={(e)=> setInput(e.target.value)}/>
-      <button>Add</button>
+      <button onClick={()=> addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo)=>(
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={()=> deleteTodo(todo.id)}>&times;</button>  
+
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
