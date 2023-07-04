a1ce228b0c2d4b8856c79a2fac7ba7dbeacf956d
junyaokh822 <junyaokh822@gmail.com>
2023-07-03 22:45:31 -0400

    update about

--- TO-Do-List/src/pages/About.jsx
+++ TO-Do-List/src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
       <div>
         <h1 className="about-title">About</h1>
         <p className="about">
-          This is a simple To-Do List application built using React. Users can add tasks to the list, delete tasks, and swap tasks status between incomplete and complete from the list. In addition, users can freely edit their tasks as needed.
+        This application is a user-friendly To-Do List built with React. It allows users to effortlessly manage their tasks by adding, deleting, and updating them. The list also offers the flexibility to mark tasks as complete or incomplete. Enjoy the convenience of organizing your tasks efficiently with this intuitive application.
         </p>
       </div>
     );
